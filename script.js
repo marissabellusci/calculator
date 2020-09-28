8a2e2d809332c3ef75977a9e218d62119aeea875
Marissa <marissabellusci@gmail.com>
2020-09-28 14:25:37 -0600

    added operator function

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function multiply(a,b){
 function divide(a,b){
     return a / b;
 }
+
+function operate(operator,a,b){
+    if (operator == add){
+       return add(a,b);
+    }
+
+    if (operator == subtract){
+        return subtract(a,b);
+    }
+
+    if (operator == multiply){
+        return multiply(a,b);
+    }
+
+    if (operator == divide){
+        return divide(a,b);
+    }
+}
